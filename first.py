9d4e74d951835ede65ef5bb0bdb9cac4cb4412a5
Ashish Kr. Pandey <akp9117409798@gmail.com>
2024-05-24 23:28:58 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,26 @@
+print("Namaste World")
+
+# variable in python 
+
+my_name = "Ashish"
+my_roll = 58
+my_clg_fee = 28000.50
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+print(x)
+print(y)
+print(z)
+
+print(my_name, my_roll, my_clg_fee)
+print(type(my_name), type(my_roll), type(my_clg_fee))
+
+a, b, c = "Orange", "Banana", "Cherry"
+print(a)
+print(b)
+print(c)
+
+# data types in python
+
