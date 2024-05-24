975817d712ee7fb525086e8c07b14beb8554c346
Ashish Kr. Pandey <akp9117409798@gmail.com>
2024-05-25 00:03:39 +0530

    variable and data types in python

--- first.py
+++ first.py
@@ -24,3 +24,48 @@ print(c)
 
 # data types in python
 
+#Str
+my_string = 'Hello World'
+print(my_string)
+print(type(my_string), 'Immutable')
+print(my_string[6])
+
+#Int
+my_integer = 999
+print(my_integer)
+print(type(my_integer), 'Immutable')
+
+#Float
+my_float = 99.34
+print(my_float)
+print(type(my_float), 'Immutable')
+
+#Complex num
+my_complex_num = 1j
+print(my_complex_num)
+print(type(my_complex_num))
+
+#List
+my_list = ["apple", "banana", "cherry"]
+print(my_list)
+print(my_list[0])
+print(my_list[1])
+print(type(my_list), 'Mutable')
+
+#Tuple
+my_tuple = ("apple", "banana", "cherry")
+print(my_tuple)
+print(my_tuple[0])
+print(type(my_tuple), 'Immutable')
+
+#Dictionary
+my_dict = {"name" : "Atul", "age" : 150}
+print(my_dict)
+print(my_dict["name"])
+print(my_dict["age"])
+print(type(my_dict), 'Mutable')
+my_dict["name"] = "Ashish"
+print(my_dict)
+print(my_dict["name"])
+
+
