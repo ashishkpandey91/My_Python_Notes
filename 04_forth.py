c0ad20cc8f043d33cc02506db6494d5aa1513e13
Ashish Kr. Pandey <akp9117409798@gmail.com>
2024-06-06 15:23:48 +0530

    list completed

--- 04_forth.py
+++ 04_forth.py
@@ -43,7 +43,107 @@ print(my_list2)
 
 print('--------------')
 
+# Add List Items
 
+my_list3 = [101, 102, 103, 104, 105, 106, 107]
+print(my_list3)
+
+my_list3.append(int(108))
+print(my_list3)
+
+my_list3.extend(my_list)
+print(my_list3)
+
+print('--------------')
+
+# Add elements of a tuple to a list:
+
+my_tuple = ('T1', 'T2', "T3", "T4")
+my_list3.extend(my_tuple)
+print(my_list3)
+
+print('--------------')
+
+# Remove List Items
+
+my_list4 = ['R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8', 'R9', "R10"]
+print(my_list4)
+
+my_list4.remove("R1")
+print(my_list4)
+
+my_list4.pop()
+print(my_list4)
+
+my_list4.pop(1)
+print(my_list4)
+
+del my_list4[0]
+print(my_list4)
+
+# Clear the list content:
+my_list4.clear()
+print(my_list4)
+
+# Delete the entire list:
+# del my_list4 
+# print(my_list4)
+
+print('--------------')
+
+# List comprehension
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+print(fruits)
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
+
+#Another...
+newlist1 = [x for x in fruits if "a" in x]
+print(newlist1)
+
+print('--------------')
+
+# Sort Lists
+
+my_list5 = [5, 10, 2, 7, 1, 3, 4, 9, 8, 6]
+print(my_list5)
+
+my_list5.sort()
+print(my_list5)
+
+print('--------------')
+
+# Make a copy of a list with the copy() method:
+
+my_list6 = my_list5.copy()
+print(my_list6)
+
+# Make a copy of a list with the list() method:
+
+my_list7 = list(my_list5)
+print(my_list7)
+
+print('--------------')
+
+#  Join Lists
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+# list1.extend(list2)
+# print(list1)
+
+# for x in list2:
+#   list1.append(x)
+# print(list1)
+
+list3 = list1 + list2
+print(list3)
 
 
 
